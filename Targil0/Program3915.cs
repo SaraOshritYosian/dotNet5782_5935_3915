0ba4c778804e9cb8145775412cf9ae8bbc1b91c6
SaraOshritYosian <saraoshr@g.jct.ac.il>
2021-10-10 20:28:54 +0300

    Initial code+Welcome functions

---
+++ Targil0/Program3915.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3915();
+            Welcome5935();
+            Console.ReadKey();
+            
+
+
+        }
+        static partial void Welcome5935();
+
+        private static void Welcome3915()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
