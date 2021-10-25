1d982dc31685e95777e2de0adef6f68293813d35
SaraOshritYosian <saraoshr@g.jct.ac.il>
2021-10-25 13:09:43 +0300

    start

--- DAL/Drone.cs
+++ DAL/Drone.cs
@@ -9,6 +9,12 @@ namespace DAL
         public struct Drone//מייצג רחפן
         {
 
+            public string Id { get; set; }
+            public string Model { get; set; }
+            public string Name { get; set; }
+            public string Weight { get; set; }
+            public string Battery { get; set; }
+
 
 
         }
