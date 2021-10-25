436bc4cc7a7038a2d3e4a59af327661408ed29ca
chanicoh <chanicoh@g.jct.ac.il>
2021-10-25 13:05:59 +0300

    ככעוד

--- DAL/Enums.cs
+++ DAL/Enums.cs
@@ -7,4 +7,5 @@ namespace DAL
    //enums סדרות של 
    public enum WeightCategories { Light,Medium,Heavy}//סוג החבילה
    public enum Priority { simple, quick, emergency }//עדיפות למשלוח
+    //פה צריך עוד 1
 }
