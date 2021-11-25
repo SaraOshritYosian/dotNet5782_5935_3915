d997beadf79a6ff94f2537245bf8a31ed1775e28
chanicoh <chanicoh@g.jct.ac.il>
2021-11-25 21:31:57 +0200

    DD

---
+++ BL/DroneBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    namespace BO
+    {
+        public class DroneBL
+        {
+            public int Id { get; set; }
+            public string Model { get; set; }
+            public Status StatusDrone { get; set; }
+            public WeightCategories Weight { get; set; }
+
+        }
+    }
+}
--- BL/DroneChargBL.cs
+++ BL/DroneChargBL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BL
 {
-    class Drone
-    {רר
+    class DroneChargBL
+    {
     }
 }
