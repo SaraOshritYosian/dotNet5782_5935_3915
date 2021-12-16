4f6bb5be24509e1dad519dd557c0889087ee955e
chanicoh <chanicoh@g.jct.ac.il>
2021-12-16 22:59:26 +0200

    SWQD

--- DAL/DalObject/DataSource.cs
+++ DAL/DalObject/DataSource.cs
@@ -18,7 +18,6 @@ namespace DalObject
         internal static List<DroneCharge> droneChargeList = new List<DroneCharge>();//רשימה של עמדות טעינה
         internal static Random rand = new Random(DateTime.Now.Millisecond);//add current time
 
-
         internal class Config
         {
 
@@ -33,44 +32,21 @@ namespace DalObject
             internal static int amountParcelId = 0;//כמות חבילות
             //מונה לחבילות והמונה ות"ז של חבילות יהיו זהים
         }
-        private static void creatDrone(int num)//פונקציה שיוצרת רחפנים create a drone
-        {
-            Config.amountDorneId++; //.... באחד amountDorneId שיוצרים רחפן זה מגדיל את
-        }
-
-        private static void creatStation(int num)//פונקציה שיוצרת תחנה create a station
-        {
-            Config.amountStationId++; //.... באחד amountStationId שיוצרים רחפן זה מגדיל את
-        }
-
-        private static void creatCustomer(int num)//פונקציה שיוצרת לקוח create a customer
-        {
-            Config.amountCustomerId++; //.... באחד amountCustomerId שיוצרים רחפן זה מגדיל את
-        }
-
-        private static void creatParcel(int num)//פונקציה שיוצרת חבילה create a parcel
-        {
-            Config.amountParcelId++; //.... באחד amountParcelId שיוצרים רחפן זה מגדיל את
-        }
-
-        private static void creatDroneCharge(int num)//פונקציה שיוצרת עמדות טעינה create a charge slot
-        {
-            Config.amountDorneId++; //.... באחד amountDorneId שיוצרים רחפן זה מגדיל את
-        }
+       
         internal static void Initialize()//מאתחל את הרשימות וקורא לכל היצירות 
         {
             dronsList.Add(new Drone() { Id = 0, Model = "2MO3",  Weight = WeightCategories.Light });
             dronsList.Add(new Drone() { Id = 1, Model = "V78K",Weight = WeightCategories.Heavy });
             dronsList.Add(new Drone() { Id = 2, Model = "5TD7", Weight = WeightCategories.Medium });
             dronsList.Add(new Drone() { Id = 3, Model = "FS001", Weight = WeightCategories.Medium });
-            stationsList.Add(new Station() { Name = 123, Id = 1, ChargeSlots = 6, Longitude = 0.3340, Latitude = 9.33340 });
-            stationsList.Add(new Station() { Name = 1234, Id = 2, ChargeSlots = 30, Longitude = 111.23, Latitude = 43.99 });
+            stationsList.Add(new Station() { Name = 1222, Id = 1, ChargeSlots = 6, Longitude = 0.3340, Latitude = 9.33340 });
+            stationsList.Add(new Station() { Name = 77834, Id = 2, ChargeSlots = 30, Longitude = 111.23, Latitude = 43.99 });
             customerList.Add(new Customer() { Id = 32424593, Name = "chana", Lattitude = 33.44, Longitude = 544.554, Pone = "0504378982" });
           //  parcelList.Add(new Parcel() { Id = Config.amountParcelId++, Senderld = 0, Targetld = 0, Weight = 0, Priority = 0, Droneld = 0, Requested = DateTime.Now, Scheduled = DateTime.Now, PichedUp = DateTime.Now, Delivered = DateTime.Now });
-            Config.amountStationId++;
-            Config.amountStationId++;
-            Config.amountCustomerId++;
-            Config.amountDorneId+=4;
+            //Config.amountStationId++;
+           // Config.amountStationId++;
+           // Config.amountCustomerId++;
+           // Config.amountDorneId+=4;
 
         }
 
--- PL/DronWindow.xaml.cs
+++ PL/DronWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using IBL.BO;
 namespace PL
 {
     /// <summary>
@@ -19,16 +19,42 @@ namespace PL
     /// </summary>
     public partial class DronWindow : Window
     {
+        static int idDrone = 0;
         IBL.BL accseccBL2;
         public DronWindow(IBL.BL accseccBL1)
         {
             InitializeComponent();
-        accseccBL2=accseccBL1;
-    }
+            accseccBL2 = accseccBL1;
+            ComboBoxWeight.Items.Add("Light");
+            //ComboBoxWeight.ItemsSource(accseccBL2.)
+            ComboBoxWeight.Items.Add("Medium");
+            ComboBoxWeight.Items.Add("Heavy");
+            List<int> aa = new List<int>();
+           for(int i = 0; i < accseccBL2.AvailableStationToChargeList().Count(); i++)
+            {
+                ComboBoxStation.Items.Add(accseccBL2.AvailableStationToChargeList().ElementAt(i).Id);
+            }
+            
+        }
+   
 
         private void ButtonAdd_Click(object sender, RoutedEventArgs e)
         {
+            Drone drone1=new Drone();
+            drone1.Id = idDrone;//id
+            idDrone++;
+            drone1.Model = (Int32.Parse(TexModel.Text)).ToString();//model
+            drone1.Weight = (Enums.WeightCategories)ComboBoxWeight.SelectedIndex;//weight
+            try
+            {
+                accseccBL2.AddDrone(drone1, ComboBoxStation.SelectedIndex);//station
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Operation Failure", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
 
+            // this.Close();
         }
 
         private void ButtonCancel_Click(object sender, RoutedEventArgs e)
@@ -40,7 +66,9 @@ namespace PL
 
         private void ComboBoxWeight_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
+            ComboBoxWeight.Items.Add("Light");
+            ComboBoxWeight.Items.Add("Medium");
+            ComboBoxWeight.Items.Add("Heavy");
         }
     }
 }
